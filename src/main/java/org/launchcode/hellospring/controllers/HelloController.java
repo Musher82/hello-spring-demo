fee01d6e1aa1ffa23f7b6e026d38cbd6a288d42b
Micah <mrusher@gmail.com>
2021-06-23 18:27:48 -0500

    Completed chapter code along

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,54 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *Created by Micah Usher
+ */
+@Controller
+@ResponseBody
+@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+public class HelloController {
+
+   //Handles request at path /hello
+   // @GetMapping("hello")
+   // @ResponseBody
+   // public String hello(){
+   //     return "Hello, Spring";
+   // }
+
+    //now lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye(){
+        return "Goodbye, Spring";
+    }
+
+    // Handles request of the form /hello?name=LaunchCode
+    //now lives at /hello/hello
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    // Handles request of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    //now lives at /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post'>" +
+                "<input type = 'text' name = 'name'>" +
+                "<input type= 'submit' value = 'Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
