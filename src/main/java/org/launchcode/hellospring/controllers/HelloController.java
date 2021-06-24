cc6948a350db21a1c808c8f53fbcee12a73797e7
Micah <mrusher@gmail.com>
2021-06-24 16:50:25 -0500

    Added language feature for exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -45,10 +45,44 @@ public class HelloController {
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" +  // submit a request to /hello
                 "<input type = 'text' name = 'name'>" +
+                "<select name = 'language'><option value = 'english' label = 'English'/><option value = 'french' label = 'French'/><option value = 'italian' label = 'Italian'/><option value = 'spanish' label = 'Spanish'/><option value = 'german' label = 'German'/>" +
                 "<input type= 'submit' value = 'Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null){
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello,";
+        }
+            else if (l.equals("french")){
+                greeting = "Bonjour,";
+            }
+            else if (l.equals("italian")){
+                greeting = "Bonjourno,";
+        }
+            else if (l.equals("spanish")){
+                greeting = "Hola,";
+        }
+            else if (l.equals("german")){
+                greeting = "Hallo,";
+        }
+            return greeting + " " + n + "!";
     }
 
 }
